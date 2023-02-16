7afe23df1759678bec5071f9ad994d676526321b
gain8823 <gain_8823cyber@msn.com>
2023-02-16 17:29:46 +0700

    Class 6 Authorization

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const connectDB = require('./config/db');
 //Route files
 const hospitals = require('./routes/hospitals');
+const auth      = require('./routes/auth');
+
+const cookieParser = require('cookie-parser');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
+connectDB();
+
 const app = express();
+
+app.use(cookieParser());
+
+app.use(express.json());
 
 // Mount router
 app.use('/api/v1/hospitals',hospitals);
-
-// app.get('/api/v1/bookings', (req,res) => {
-
-//     res.status(200).json({ success:true , msg:''});
-
-// });
-
-// app.get('/api/v1/auth', (req,res) => {
-
-//     res.status(200).json(({ success:true , data:{id:1}}));
-
-// });
-
-// app.get('/api/v1/users', (req,res) => {
-
-//     res.status(200).json(({ success:true , data:{id:1}}));
-
-// });
+app.use('/api/v1/auth',auth);
 
 const PORT = process.env.PORT || 2000;
 
-app.listen(PORT, console.log('Server is running in', process.env.NODE_ENV, ' mode on port ', PORT));
+const server = app.listen(PORT, console.log('Server is running in', process.env.NODE_ENV, ' mode on port ', PORT));
+
+process.on('unhandledRejection',(err,promise)=>{
+
+    console.log(`Error: ${err.message}`);
+
+    server.close(()=>process.exit(1));  
+
+});
